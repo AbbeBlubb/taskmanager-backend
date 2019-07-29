67e340faf69c8086793a1ce46b2bbc0946774580
Alberto Chávez Pérez <andalusier@pm.me>
2019-07-30 01:53:29 +0200

    Server settings

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-var express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+require('dotenv').config();
 const users = require('./users');
 
 const app = express();
-
-var port = process.env.PORT || 3000;
-
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.set('json spaces', 2);
+
+var currentdate = new Date(); 
+var time = currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/taskmanager-backend"
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+mongoose.Promise = global.Promise;
+mongoose.connection.on("error", err => console.error("Connection error:", err))
+mongoose.connection.once("open", () => console.log("## taskmanager-backends is connected to mongodb ##"))
+
+
+
+
+
 
 app.get('/users', function(req, res){
   res.json(users)
@@ -15,5 +32,10 @@ app.get('/users/:id', function(req, res){
   res.json({key: {second:'One specific user'}})
 })
 
+app.post('/users', function(req, res){
+  console.log(req.body)
+  res.json(req.body)
+})
 
-app.listen(port);
+const port = process.env.PORT || 3000;
+app.listen(port, function(){console.log("## Listening on port "+ port +" ##" + "\n## Time is: " + time + " ##")});
